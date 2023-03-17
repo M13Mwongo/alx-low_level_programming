7978f034a8105a8bdd0c16f038b48c3af63fc3fc
MMMwiti <mwongomwiti@gmail.com>
2023-03-16 18:48:51 -0700

    Task 4 attempt

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Declare variables*/
+	char statement[] ="Programming is like building a multilingual puzzle";
+	
+	/*statements*/
+	puts(statement);
+
+	return 0;
+}
