0f39cc89ca10f0e217709c9680a2984266ba5cb6
MMMwiti <mwongomwiti@gmail.com>
2023-03-17 04:21:46 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
