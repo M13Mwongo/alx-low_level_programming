595701a02cbd47b667f7c7089fc09546bb0a8086
MMMwiti <mwongomwiti@gmail.com>
2023-03-21 06:52:10 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,4 @@
-#include <stdio.h>
-/**
- * main - entry point
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
+#include "main.h"
 /**
  * _islower - checks if a character is a lower case
  *
