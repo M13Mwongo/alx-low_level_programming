f05e9f4758d26752ed69eb93417e9d769aadf792
MMMwiti <mwongomwiti@gmail.com>
2023-03-21 06:30:36 -0700

    mainh modified

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,6 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-/**
- * _putchar - writes the character c to std
- * @c: parameter to compare
- * Return: 1 (success) or 0 (failure)
- */
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
 
 
 #endif
