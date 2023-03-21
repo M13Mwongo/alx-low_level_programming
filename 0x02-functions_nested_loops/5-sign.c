708b493c62fb42a9460bac9069cbe6b0791f7c7e
MMMwiti <mwongomwiti@gmail.com>
2023-03-21 07:28:28 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _sign - prints the sign of a number
+ *@n: num to check
+ *
+ * Return: 1 (positive), 0(zero), -1 (negative)
+ */
+int _sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
