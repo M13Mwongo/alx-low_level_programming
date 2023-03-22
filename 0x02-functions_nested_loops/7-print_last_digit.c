da8d3a2fc79ab19ceecac90ae6e9d0990aa8c321
MMMwiti <mwongomwiti@gmail.com>
2023-03-21 17:55:34 -0700

    task 6 correction and task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit of a number
+ * @i: integer being analysed
+ *
+ * Return: Last digit of number
+ */
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
