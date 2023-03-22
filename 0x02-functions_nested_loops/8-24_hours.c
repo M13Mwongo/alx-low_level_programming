fecb7b3c8d00e57732c2e00215da3b9545a20d1c
MMMwiti <mwongomwiti@gmail.com>
2023-03-21 18:14:06 -0700

    task 8 trial

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: 0 (success)
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i);
+			_putchar(':');
+			_putchar(j);
+			_putchar('\n');
+		}
+	}
+	return (0);
+}
