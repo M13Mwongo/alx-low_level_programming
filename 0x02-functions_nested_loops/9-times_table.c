afb89f7f2ff9f669a781ded86706df19655e124d
MMMwiti <mwongomwiti@gmail.com>
2023-03-21 19:54:38 -0700

    task 9 new attempt

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,28 +7,26 @@
  */
 void times_table(void)
 {
-	int i,j,product;
+	int i, j, product;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			product = i * j;
-			
 			if (j == 0)
 			{
-				_putchar('0');
+				printf("0");
 			}
 			else if (j <= 9)
 			{
-				printf(",  %d", product);
-			}
-			else 
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(product / 10 + '0');
-				_putchar(product % 10 + '0');
+				if (prpduct <= 9)
+				{
+					printf(",  %d", product);
+				} else
+				{
+					printf(", %d", product);
+				}
 			}
 		}
 		printf("\n");
