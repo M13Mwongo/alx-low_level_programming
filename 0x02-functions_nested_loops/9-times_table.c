a76a052bd60c40f3acf36fb62070cb99bfff4201
MMMwiti <mwongomwiti@gmail.com>
2023-03-21 18:50:57 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - prints the entirety of the times tables of 9
+ *
+ * Return: null
+ */
+void times_table(void)
+{
+	int i,j,product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
+			
+			if (j == 0)
+			{
+				_putchar('0');
+			}
+			else if (j <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(product + '0');
+			}
+			else 
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
