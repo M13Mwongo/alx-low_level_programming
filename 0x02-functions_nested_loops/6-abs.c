6309ab3e12bb1779f0eae1c4302f444e125a317c
MMMwiti <mwongomwiti@gmail.com>
2023-03-21 17:49:55 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+/**
+ * _abs - computes the absolute value of an integer
+ * @i: int to get absolute value
+ *
+ * Return: 0 or number greater than 0 (success)
+ */
+int _abs(int i)
+{
+	return abs(i);
+}
