d23147517c66265212303701dfb096a74baeb420
MMMwiti <mwongomwiti@gmail.com>
2023-03-23 12:05:06 -0700

    task 1 trial

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - test positive_or_negative function
+ *
+ * Return: 0 for success
+ */
+void main(void)
+{
+	positive_or_negative();
+	return (0);
+}
+
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+ALX Debugging Course in C
