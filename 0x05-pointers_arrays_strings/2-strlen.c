93a518b4010465a1a8eb04d1ee1b2391dcf87c69
MMMwiti <mwongomwiti@gmail.com>
2023-03-27 07:24:36 -0700

    task 2 done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be checked
+ *
+ * Return: string length
+ */
+void _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
