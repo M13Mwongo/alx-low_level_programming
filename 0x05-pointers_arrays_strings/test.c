302037635ea859b642e278d02f492775403dfffc
MMMwiti <mwongomwiti@gmail.com>
2023-03-27 07:59:04 -0700

    corrected declaration of int

--- 0x05-pointers_arrays_strings/test.c
+++ 0x05-pointers_arrays_strings/test.c
@@ -1,13 +1,16 @@
 #include "main.h"
-/*
- * main - test functions
- * Return: 0
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	char s[10] = "My School";
+    char s[10] = "My School";
 
-    	printf("%s\n", s);
+    printf("%s\n", s);
     rev_string(s);
     printf("%s\n", s);
     return (0);
