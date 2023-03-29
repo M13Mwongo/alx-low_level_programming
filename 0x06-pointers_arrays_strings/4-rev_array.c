e552483c414169a17189036dd7aea101660bf55c
MMMwiti <mwongomwiti@gmail.com>
2023-03-29 09:37:58 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * reverse_array - reverses the array
+ * @a: array in question
+ * @n: number of elements in the array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int hold, start;
+	int finish = n - 1;
+
+	for (start = 0; start < n; start++)
+	{
+		hold = *(a + start);
+		*(a + start) = *(a + finish);
+		*(a + finish) = hold;
+		finish--;
+	}
+}
