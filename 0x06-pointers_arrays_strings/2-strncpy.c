28144561273a6d18036cf7296ba5d24607e55916
MMMwiti <mwongomwiti@gmail.com>
2023-03-31 02:40:55 -0700

    task 2 correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,7 +4,7 @@
  * _strncpy - copies a string
  * @dest: destination of string
  * @src: string to be copied
- * @n: bytes
+ * @n: number of characters to be copied from source
  *
  * Return: copied string
  *
@@ -13,9 +13,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
+		if (i == n-1)
+		{
+			dest[i + 1] = '\0';
+		}
 	}
 	return (dest);
 }
