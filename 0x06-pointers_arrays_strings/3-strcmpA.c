dbf03f22fada277402a098c44082eba59df6834d
MMMwiti <mwongomwiti@gmail.com>
2023-03-31 03:03:50 -0700

    task 3 redo

--- 0x06-pointers_arrays_strings/3-strcmpA.c
+++ 0x06-pointers_arrays_strings/3-strcmpA.c
@@ -4,10 +4,15 @@
  * @s1: string one
  * @s2: string two
  *
- * Return: 0 (equal), < 0 (s1 < s2), > 0 (s1 > s2)
+ * Return: 0 (equal strings), < 0 (s1 < s2), > 0 (s1 > s2)
  */
 int _strcmp(char *s1, char *s2)
 {
+	while (*s1 != '\0' && *s != '\0' && *s1 != *s2)
+	{
+		s1++;
+		s2++;
+	}
 	if (*s1 == *s2)
 	{
 		return (0);
