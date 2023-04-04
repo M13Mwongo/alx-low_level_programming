da055efde9ffee8de23c1773bc98d55a8b701921
MMMwiti <mwongomwiti@gmail.com>
2023-04-04 04:20:38 -0700

    task 8 done

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,12 +8,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
+	int i, sumLR = 0, sumRL = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + i);
-		sum2 += *(a + i + size);
+		sumLR += *(a + i * size + i);
+		sumRL += *(a + i * size + (size - i - 1));
 	}
-	printf("%d, %d\n", sum1, sum2);
+	printf("%d, %d\n", sumLR, sumRL);
 }
