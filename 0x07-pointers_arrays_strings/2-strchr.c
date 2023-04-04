32dc42cfc17c4de6856fcfa62cd196199246ee25
MMMwiti <mwongomwiti@gmail.com>
2023-04-03 18:16:51 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - return pointer to first occurence of char c, or NULL if not found
+ * @s: pointer to char
+ * @c: character being looked for
+ *
+ * Return: pointer if found, or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (NULL);
+}
