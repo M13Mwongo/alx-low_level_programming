87fbe763a8a998b295abe3b3b9ec06953199033a
MMMwiti <mwongomwiti@gmail.com>
2023-04-03 17:49:32 -0700

    task 1 mod

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,10 +10,12 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+	char *pd = dest;
+	char *ps = src;
 
 	for (i = 0; i < n; i++)
 	{
-		*dest++ = *src++;
+		*pd++ = *ps++;
 	}
 	return (dest);
 }
