a9a25b4e938b611495407e7e523f40db565a6ae7
MMMwiti <mwongomwiti@gmail.com>
2023-04-13 01:43:57 -0700

    mods

---
+++ 0x09-static_libraries/strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - copies string
+ * @dest: destination of copied string
+ * @src: source of string
+ *
+ * Return: copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
