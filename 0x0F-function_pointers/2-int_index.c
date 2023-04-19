989bbb212a0c657d4bccbb99ca41b76d9ebad5b7
MMMwiti <mwongomwiti@gmail.com>
2023-04-19 16:50:11 -0700

    mods

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,26 +9,29 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = -1;
 
-	if (size < 1)
+	if (array && cmp)
 	{
-		return (-1);
-	}
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]))
+		if (size < 1)
 		{
-			break;
+			return (-1);
 		}
-		else
+		for (i = 0; i < size; i++)
 		{
-			continue;
+			if (cmp(array[i]))
+			{
+				break;
+			}
+			if (cmp(array[i]) == -1)
+			{
+				return (-1);
+			}
 		}
-	}
-	if (i == (size - 1))
-	{
-		return (-1);
+		if (i == (size - 1) || i == size)
+		{
+			return (-1);
+		}
 	}
 	return (i);
 }
