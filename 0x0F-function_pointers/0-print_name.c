4522ca4aaa335315d457a93fb4260a756375c59d
MMMwiti <mwongomwiti@gmail.com>
2023-04-19 15:57:59 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name in question
+ * @f: function pointer to alter name
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+	f(name);
+}
