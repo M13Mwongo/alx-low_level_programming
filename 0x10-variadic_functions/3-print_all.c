354fb71c6c66f7777efac2d94e06a4a816022cd5
MMMwiti <mwongomwiti@gmail.com>
2023-04-20 12:42:42 -0700

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints anything
+ * @format: format of data being passed
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list data;
+	int i;
+	char c;
+	char *s;
+	double f;
+	unsigned int ui = 0;
+
+	va_start(data, format);
+	while (format[ui])
+	{
+		switch (format[ui])
+		{
+			case 'c':
+				c = va_arg(data, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				i = va_arg(data, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = va_arg(data, double);
+				printf("%f", f);
+				break;
+			case 's':
+				s = va_arg(data, char*);
+				if (s != NULL)
+				{
+					printf("%s", s);
+				}
+				else
+				{
+					printf("(nil)");
+				}
+				break;
+			default:
+				break;
+		}
+		ui++;
+		if (format[ui] != '\0' && (format[ui] == 'c' || format[ui] == 'i' || format[ui] == 'f' || format[ui] == 's'))
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
