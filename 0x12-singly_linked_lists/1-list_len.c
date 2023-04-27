ee1b72393dc6e3e0f74f6fd982676c4d08ecfc76
MMMwiti <mwongomwiti@gmail.com>
2023-04-27 16:02:08 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - finds length of list
+ * @h: list
+ * Return: number of elements in list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
