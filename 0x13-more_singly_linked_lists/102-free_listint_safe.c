ec5e96ef2b27bcd7c6661d975029cf8fd0355204
MMMwiti <mwongomwiti@gmail.com>
2023-05-02 15:41:27 -0700

    task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,31 @@
+#include  "lists.h"
+/**
+ * free_listint_safe - free list
+ * @h: linked list
+ * Return: size of list freed
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t size = 0;
+	listint_t *current, *temp;
+
+	if (h == NULL || *h == NULL)
+	{
+		return (size);
+	}
+	current = *h;
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+		size++;
+		if (temp <= current)
+		{
+			*h = NULL;
+			exit(98);
+		}
+	}
+	*h = NULL;
+	return (size);
+}
