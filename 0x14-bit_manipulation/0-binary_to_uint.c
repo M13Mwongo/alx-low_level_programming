f2f40e801b658c0141c63c9b4097506a0b7c3ca0
MMMwiti <mwongomwiti@gmail.com>
2023-05-04 13:35:32 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary number to insigned integer
+ * @b: binary mumber
+ * Return: unsigned int | 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+	int i;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+			result = result << 1;
+			result |= b[i] - '0';
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (result);
+}
